00985e0a1d9aeffde3c40a06b39d8a0b9cc2f6da
Bryan Cunningham <bryan.cunningham@me.com>
2025-05-09 08:30:12 -0700

    update responsive logic

--- libs/components/src/stepper/stepper.component.ts
+++ libs/components/src/stepper/stepper.component.ts
@@ -1,6 +1,6 @@
-import { CdkStepper } from "@angular/cdk/stepper";
+import { CdkStepper, StepperOrientation } from "@angular/cdk/stepper";
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 import { ResizeObserverDirective } from "../resize-observer/resize-observer.directive";
 
@@ -20,6 +20,36 @@ export class StepperComponent extends CdkStepper {
     [4, 900],
   ]);
 
+  private internalOrientation: StepperOrientation = undefined;
+  private initialOrientation: StepperOrientation = undefined;
+
+  // overriding CdkStepper orientation input so we can default to vertical
+  @Input()
+  override get orientation() {
+    return this.internalOrientation;
+  }
+  override set orientation(value: StepperOrientation) {
+    if (!this.internalOrientation) {
+      // tracking the first value of orientation. We want to handle resize events if it's 'horizontal'.
+      // If it's 'vertical' don't change the orientation to 'horizontal' when resizing
+      this.initialOrientation = value;
+    }
+
+    this.internalOrientation = value;
+  }
+
+  handleResize(entry: ResizeObserverEntry) {
+    if (this.initialOrientation === "horizontal") {
+      const width = entry.contentRect.width;
+      const numberOfSteps = this.steps.length;
+
+      this.orientation =
+        width < this.resizeWidthsMap.get(numberOfSteps) ? "vertical" : "horizontal";
+      // This is a method of CdkStepper. Their docs define it as: 'Marks the component to be change detected'
+      this._stateChanged();
+    }
+  }
+
   isStepDisabled(index: number) {
     if (this.selectedIndex !== index) {
       return this.selectedIndex === index - 1
@@ -31,20 +61,6 @@ export class StepperComponent extends CdkStepper {
 
   selectStepByIndex(index: number): void {
     this.selectedIndex = index;
-  }
-
-  private initialOrientation = this.orientation;
-
-  handleResize(entry: ResizeObserverEntry) {
-    const width = entry.contentRect.width;
-    const numberOfSteps = this.steps.length;
-
-    if (this.initialOrientation === "horizontal") {
-      this.orientation =
-        width < this.resizeWidthsMap.get(numberOfSteps) ? "vertical" : "horizontal";
-      // This is a method of CdkStepper. Their docs define it as: 'Marks the component to be change detected'
-      this._stateChanged();
-    }
   }
 
   /**
